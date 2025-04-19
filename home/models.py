d736ed85c703964b5704108cf4d162f6170cb9a7
José Anderson <freefiremitodino@gmail.com>
2025-04-18 23:36:01 -0300

    versao 4.5

--- home/models.py
+++ home/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
-
 class Receita(models.Model):
     titulo = models.CharField(max_length=50)
     descricao = models.TextField(max_length=150)
     ingredients = models.TextField()
     preparo = models.TextField()
     imagem = models.ImageField(upload_to='receitas/', null=True, blank=True)
+    video_url = models.URLField(null=True, blank=True, help_text="Insira a URL do vídeo de preparo (YouTube, etc.)")
 
     def __str__(self):
         return self.titulo
